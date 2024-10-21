5e33c5ea73704e71aa7139d97927299b06d1e7d6
Samuel Imanol <167726711+Samueljustiniani@users.noreply.github.com>
2024-10-21 01:12:07 -0500

    pichi poto

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
-const ip = '3.218.160.181';
+const ip = '3.218.160.181'; // Tu IP pública en AWS
 
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static('public'));
 
 // Manejar la ruta raíz
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Corregido
+    res.sendFile(path.join(__dirname, 'css', 'img', 'js', 'pages' , 'index.html'));
 });
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Server en http://${ip}:${PORT}`);
+    console.log(`Server en http://${ip}:${PORT}`); // Usamos tu IP en el log
 });
