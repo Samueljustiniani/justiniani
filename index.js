c10e18a8d9a5032e097a7a4f977feededf55fabf
Samuel Imanol <167726711+Samueljustiniani@users.noreply.github.com>
2024-10-21 00:47:57 -0500

    correcion

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 const PORT = 3000;
-const ip = '3.218.160.181'
+const ip = '3.218.160.181';
 
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static('public'));
 
-app.use(express.static('public'))
+// Manejar la ruta raíz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Corregido
+});
 
-
-app.get('/',(req,res) =>{
-    res,sendFile(path,join(__dirname, 'index.html'))
-} )
-
-
+// Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Server en http://${ip}:${PORT}`);
 });
